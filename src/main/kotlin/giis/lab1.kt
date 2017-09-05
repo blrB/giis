d5b2e231e675f6b3992db13bdb3d96dca5a2b5d6
Bobkov Andrey <bobr8949@yandex.ru>
2017-09-05 22:35:23 +0300

    click on canvas

--- src/main/kotlin/giis/lab1.kt
+++ src/main/kotlin/giis/lab1.kt
@@ -1,40 +1,54 @@
 package giis
 
+import org.w3c.dom.CanvasRenderingContext2D
 import org.w3c.dom.HTMLButtonElement
-import org.w3c.dom.HTMLInputElement
+import org.w3c.dom.HTMLCanvasElement
 import kotlin.browser.document
 import kotlin.js.Math
 
 fun initLab1() {
     console.log("Init lab 1")
-    val button = document.getElementById("draw-lab-1") as HTMLButtonElement
-    button.onclick = {
-        console.log("Click draw button for labs 1")
-        drawDDA("DDA")
-        drawBresenham("Bresenham")
-        drawWu("Wu")
+
+    val buttonDDA = document.getElementById("dda") as HTMLButtonElement
+    buttonDDA.onclick = {
+        drawLine("canvas", ::drawDDA)
+    }
+    console.log("Init buttonDDA")
+
+    val buttonBresenham = document.getElementById("bresenham") as HTMLButtonElement
+    buttonBresenham.onclick = {
+        drawLine("canvas", ::drawBresenham)
+    }
+    console.log("Init buttonBresenham")
+
+    val buttonWu = document.getElementById("wu") as HTMLButtonElement
+    buttonWu.onclick = {
+        drawLine("canvas", ::drawWu)
+    }
+    console.log("Init buttonWu")
+
+}
+
+fun drawLine(elementId: String, drawAlgorithm: (p1: Coordinate, p2: Coordinate, canvas: HTMLCanvasElement) -> Unit) {
+    val canvas = document.getElementById(elementId) as HTMLCanvasElement
+    val points = arrayListOf<Coordinate>()
+
+    canvas.onclick = {
+        val pos = getMousePosOnCanvas(canvas, it)
+        points.add(pos)
+        if (points.size == 2) {
+            drawAlgorithm(points[0], points[1], canvas)
+            points.clear()
+            canvas.onclick = null
+        }
     }
 }
 
-fun getCoordinates(): Coordinates {
-    val inputX1 = document.getElementById("x1") as HTMLInputElement
-    val x1 = inputX1.value.toDouble()
-    val inputY1 = document.getElementById("y1") as HTMLInputElement
-    val y1 = inputY1.value.toDouble()
-    val inputX2 = document.getElementById("x2") as HTMLInputElement
-    val x2 = inputX2.value.toDouble()
-    val inputY2 = document.getElementById("y2") as HTMLInputElement
-    val y2 = inputY2.value.toDouble()
-    return Coordinates(x1, y1, x2, y2)
-}
-
-
-fun drawDDA(elementId: String) {
-    val context = getCanvasContext(elementId)
-    context.clearRect(0.0, 0.0, 150.0, 150.0)
-    val coordinates = getCoordinates()
-    val (x1, y1, x2, y2) = coordinates
-    console.log("Draw DDA: ($x1;$y1) and ($x2;$y2)")
+fun drawDDA(source: Coordinate, target: Coordinate, canvas: HTMLCanvasElement) {
+    val (x1, y1) = source
+    val (x2, y2) = target
+    val context = canvas.getContext("2d") as CanvasRenderingContext2D
+    console.log("\nDraw DDA: ($x1;$y1) and ($x2;$y2)")
 
     val length = Math.max(Math.abs(x2 - x1), Math.abs(y2 - y1))
     val dx = (x2 - x1) / length
@@ -43,31 +57,120 @@ fun drawDDA(elementId: String) {
     var x = x1 + 0.5 * Math.sign(dx)
     var y = y1 + 0.5 * Math.sign(dy)
 
-    context.drawPixel(x.toInt(),y.toInt())
+    context.drawPixel(x.toInt(), y.toInt())
     console.log("Step 0: x:$x; y:$y, Plot(${x.toInt()},${y.toInt()})")
     var i = 1
-    while (i <= length){
+    while (i <= length) {
         x += dx
         y += dy
-        context.drawPixel(x.toInt(),y.toInt())
+        context.drawPixel(x.toInt(), y.toInt())
         console.log("Step $i: x:$x; y:$y, Plot(${x.toInt()},${y.toInt()})")
         i++
     }
-
 }
 
-fun drawBresenham(elementId: String) {
-    var context = getCanvasContext(elementId)
-    context.clearRect(0.0, 0.0, 150.0, 150.0)
-    var coordinates = getCoordinates()
-    val (x1, y1, x2, y2) = coordinates
-    console.log("Draw Bresenham: ($x1;$y1) and ($x2;$y2)")
+fun drawBresenham(source: Coordinate, target: Coordinate, canvas: HTMLCanvasElement) {
+    val (x1, y1) = source
+    val (x2, y2) = target
+    val context = canvas.getContext("2d") as CanvasRenderingContext2D
+    console.log("\nDraw Bresenham: ($x1;$y1) and ($x2;$y2)")
+
+    var x = x1
+    var y = y1
+    val dx = Math.abs(x2 - x1)
+    val dy = Math.abs(y2 - y1)
+    var e = 2 * dy - dx
+    console.log("dx: $dx; dy: $dy; e: $e")
+
+    val changeX = if (x1 < x2) 1 else -1
+    val changeY = if (y1 < y2) 1 else -1
+
+    context.drawPixel(x.toInt(), y.toInt())
+    console.log("Step 0: x:$x; y:$y, e':$e Plot(${x.toInt()},${y.toInt()})")
+    var i = 1
+    if (dx >= dy) {
+        while (i <= dx) {
+            val eOld = e
+            if (e >= 0) {
+                y += changeY
+                e -= 2 * dx
+            }
+            x += changeX
+            e += 2 * dy
+            context.drawPixel(x.toInt(), y.toInt())
+            console.log("Step $i: e:$eOld, x:$x; y:$y, e':$e Plot(${x.toInt()},${y.toInt()})")
+            i++
+        }
+    } else {
+        while (i <= dy) {
+            val eOld = e
+            if (e >= 0) {
+                x += changeX
+                e -= 2 * dy
+            }
+            y += changeY
+            e += 2 * dx
+            context.drawPixel(x.toInt(), y.toInt())
+            console.log("Step $i: e:$eOld, x:$x; y:$y, e':$e Plot(${x.toInt()},${y.toInt()})")
+            i++
+        }
+    }
 }
 
-fun drawWu(elementId: String) {
-    var context = getCanvasContext(elementId)
-    context.clearRect(0.0, 0.0, 150.0, 150.0)
-    var coordinates = getCoordinates()
-    val (x1, y1, x2, y2) = coordinates
-    console.log("Draw Wu: ($x1;$y1) and ($x2;$y2)")
+fun drawWu(source: Coordinate, target: Coordinate, canvas: HTMLCanvasElement) {
+    val (x1, y1) = source
+    val (x2, y2) = target
+    val context = canvas.getContext("2d") as CanvasRenderingContext2D
+    console.log("\nDraw Wu: ($x1;$y1) and ($x2;$y2)")
+
+    var x = x1
+    var y = y1
+    val dx = Math.abs(x2 - x1)
+    val dy = Math.abs(y2 - y1)
+    var e = dy / dx - 0.5
+    console.log("dx: $dx; dy: $dy; e: $e")
+
+    val changeX = if (x1 < x2) 1 else -1
+    val changeY = if (y1 < y2) 1 else -1
+
+    context.drawPixel(x.toInt(), y.toInt())
+    console.log("Step 0: e:$e, x:$x; y:$y, Plot(${x.toInt()},${y.toInt()})")
+    var i = 1
+    if (dx >= dy) {
+        while (i <= dx) {
+            if (e >= 0) {
+                y += changeY
+                e -= 1
+            }
+            x += changeX
+            e += dy / dx
+            context.drawPixel(x.toInt(), y.toInt())
+            val a = Math.abs(e)
+            if (e < 0) {
+                context.drawAlfaPixel(x.toInt(), y.toInt() - 1, a)
+            } else {
+                context.drawAlfaPixel(x.toInt(), y.toInt() + 1, a)
+            }
+            console.log("Step $i: e:$e, x:$x; y:$y, a:$a Plot(${x.toInt()},${y.toInt()})")
+            i++
+        }
+    } else {
+        while (i <= dy) {
+            if (e >= 0) {
+                x += changeX
+                e -= 1
+            }
+            y += changeY
+            e += dx / dy
+            context.drawPixel(x.toInt(), y.toInt())
+            val a = Math.abs(e)
+            if (e < 0) {
+                context.drawAlfaPixel(x.toInt()-1, y.toInt(), a)
+            } else {
+                context.drawAlfaPixel(x.toInt()+1, y.toInt(), a)
+            }
+            console.log("Step $i: e:$e, x:$x; y:$y, a:$a Plot(${x.toInt()},${y.toInt()})")
+            i++
+        }
+    }
 }
--- src/main/kotlin/giis/lib.kt
+++ src/main/kotlin/giis/lib.kt
@@ -2,8 +2,12 @@ package giis
 
 import org.w3c.dom.CanvasRenderingContext2D
 import org.w3c.dom.HTMLCanvasElement
-import kotlin.browser.document
+import org.w3c.dom.events.Event
+import org.w3c.dom.events.MouseEvent
 import kotlin.js.Math
+
+data class Coordinate(val x: Double,
+                       val y: Double)
 
 data class Coordinates(val x1: Double,
                        val y1: Double,
@@ -14,13 +18,22 @@ fun CanvasRenderingContext2D.drawPixel(x: Int, y: Int) {
     this.fillRect(x.toDouble(), y.toDouble(), 1.0, 1.0)
 }
 
+fun CanvasRenderingContext2D.drawAlfaPixel(x: Int, y: Int, a: Double) {
+    this.fillStyle = "rgba(0, 0, 0, $a)"
+    this.fillRect(x.toDouble(), y.toDouble(), 1.0, 1.0)
+    this.fillStyle = "rgba(0, 0, 0, 1)"
+}
+
 fun Math.sign(value: Number): Int = when {
         value.toDouble() < 0 -> -1
         value.toDouble() > 0 -> 1
         else -> 0
     }
 
-fun getCanvasContext(elementId: String): CanvasRenderingContext2D {
-    val canvas = document.getElementById(elementId) as HTMLCanvasElement
-    return canvas.getContext("2d") as CanvasRenderingContext2D
+fun getMousePosOnCanvas(canvas: HTMLCanvasElement, event: Event): Coordinate {
+    val rect = canvas.getBoundingClientRect()
+    val evt = event as MouseEvent
+    val x = evt.clientX - rect.left
+    val y = evt.clientY - rect.top
+    return Coordinate(x, y)
 }
